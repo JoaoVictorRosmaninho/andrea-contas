fd9d2f911d59e108407a2fc8ce29ea82932c5d34
WilsenyTeixeira <wilseny@outlook.com>
2022-08-29 08:16:28 -0300

    melhorando PerfilCliente

--- src/components/subcomponents/PerfilCliente.jsx
+++ src/components/subcomponents/PerfilCliente.jsx
@@ -3,8 +3,11 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { parseCookies } from 'nookies';
 import axios from "axios";
+import Table from 'react-bootstrap/Table';
+import "../css/perfil-subcomponent.css";
 
-let baseUrlContas = "http://localhost:3001/contas"
+
+const baseUrlContas = "http://localhost:3001/contas/list";
 const baseUrlCliente = "http://localhost:3001/clientes/findbyid";
 const baseUrlTotalAReceber = "http://localhost:3001/contas/gettotalareceber";
 
@@ -19,38 +22,68 @@ export default function PerfilCliente({show, hide, id}) {
             color: '#000000',
         }
         const [valuesCliente, setValuesCliente] = React.useState([]);
+        const [valuesContas, setValuesContas] = React.useState([]);
+        const [valuesTotalAReceber, setValuesTotalAReceber] = React.useState([]);
 
         React.useEffect(() => {
             id && axios.get(baseUrlCliente + `/${id}`, config)
             .then((resp) => setValuesCliente(resp.data))
             .catch((err) => console.log(err));
-        }, []);
-        /*
-        React.useEffect(() => {
-            id && axios.post(baseUrlCliente + `/${id}`,{cliente: id, ativo: true} ,config)
-            .then((resp) => setValuesCliente(resp.data))
-            .catch((err) => console.log(err));
-        }, []);*/
 
-        console.log(id);
+            id && axios.post(baseUrlContas, {cliente: id, ativo: true} , config)
+            .then((resp) => setValuesContas(resp.data))
+            .catch((err) => console.log(err));
+
+            id && axios.post(baseUrlTotalAReceber, {cliente: id, ativo: true} , config)
+            .then((resp) => setValuesTotalAReceber(resp.data))
+            .catch((err) => console.log(err));
+        }, []);
+
+        const lengthValuesContas = (array) => {
+            return ((array).length);
+        }
         return(
-            <Modal show={show} onHide={hide} style={modelStyle}>
+            <Modal show={show} size="lg" onHide={hide} style={modelStyle} className="modal1">
             <Modal.Header closeButton>
                 <Modal.Title><span>Foto de perfil aqui</span>{valuesCliente.nome}</Modal.Title>
-                
             </Modal.Header>
             <Modal.Body>
-                <span>Email: {valuesCliente.email}</span>
-                <span>Ultima Conta Aberta em: </span>
+                <span>Ultima Conta Aberta em: {lengthValuesContas(valuesContas) !== 0 ? valuesContas[0].criadoEm: ''}</span>
                 <span>Ultimo Pagamento em: </span>
-
+                <div className="tablesContas">
+                    <Table striped>
+                        <thead>
+                            <tr>
+                                <th>Observação</th>
+                                <th>Valor Atual</th>
+                                <th>Vencimento Atual</th>
+                                <th>Número de Parcelas</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                            valuesContas.map((element) => {
+                                return (
+                                    <tr>
+                                        <td>{element.observacoes}</td>
+                                        <td>{element.valorAtual}</td>
+                                        <td>{element.dataVencimentoAtual}</td>
+                                        <td>{element.numeroParcelasAtual}</td>
+                                        <td>
+                                            <Button className="table-buttons" ><a className="link">Pagar Conta</a></Button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </Table>
+                </div>
             </Modal.Body>
             <Modal.Footer>
-                <span>Total a receber: </span>
+                <span>Email: {valuesCliente.email}</span>
+                <span>Total a receber: {valuesTotalAReceber}</span>
                 <span>Telefone: {valuesCliente.telefone}</span>
-                <Button variant="secondary" onClick={hide}>
-                Close
-                </Button>
             </Modal.Footer>
             </Modal>
         )
